fe04f90166a0835211e78fcb61a8170cc840781c
Saborknight <support@saborknight.com>
2017-11-21 13:16:00 +0000

    Maintenance - Implemented "clean" task to clean `dist` before build

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var watchSass = require("gulp-watch-sass");
 var gs		= require('gulp-selectors');
+var del   = require('del');
+var run   = require('run-sequence');
 
 gulp.task("mangle", ['sass', 'bootstrap'], function() {
   gulp.src(['**/*.css', '**/*.html', '**/*.php', '**/.+(htpasswd|htaccess)', '!dist/**/*.*', '!node_modules/**/*.*'])
@@ -12,33 +14,37 @@ gulp.task("mangle", ['sass', 'bootstrap'], function() {
 gulp.task("sass", function () {
   return gulp.src('assets/scss/**/*.scss')
   .pipe(sass())
-    .pipe(gulp.dest('assets/css/'))
+    .pipe(gulp.dest('assets/css/'));
 });
 
 gulp.task("bootstrap", function() {
   return gulp.src('node_modules/bootstrap/dist/css/bootstrap.min.css')
-    .pipe(gulp.dest('assets/css/'))
+    .pipe(gulp.dest('assets/css/'));
 });
 
 gulp.task("images", function() {
   return gulp.src(['assets/images/**/*.*'])
-    .pipe(gulp.dest('dist/assets/images/'))
+    .pipe(gulp.dest('dist/assets/images/'));
 });
 
 
 gulp.task("js", function() {
   return gulp.src(['assets/js/**/*.*', 'node_modules/bootstrap/dist/js/bootstrap.min.js'])
-    .pipe(gulp.dest('dist/assets/js/'))
+    .pipe(gulp.dest('dist/assets/js/'));
 });
 
 
 gulp.task("fonts", function() {
   return gulp.src(['assets/fonts/**/*.*'])
-    .pipe(gulp.dest('dist/assets/fonts/'))
+    .pipe(gulp.dest('dist/assets/fonts/'));
 });
 
-gulp.task("build", ['mangle', 'images', 'js', 'fonts'],  function() {
+gulp.task("clean", function() {
+  return del(['dist/']);
+});
 
+gulp.task("build", ['clean'],  function() {
+  run(['mangle', 'images', 'js', 'fonts']);
 });
 
 gulp.task("sass:watch", ['sass'], function(){
@@ -46,6 +52,6 @@ gulp.task("sass:watch", ['sass'], function(){
     'assets/scss/**/*.scss'
   ])
   .pipe(sass())
-  .pipe(gulp.dest("assets/css"))
+  .pipe(gulp.dest("assets/css"));
 });
 //
