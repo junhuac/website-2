66911288e1d1c8acba43b442ec7601dc735247aa
Saborknight <support@saborknight.com>
2017-11-22 16:04:19 +0000

    Feature - Implement ftp capabilities to development env
    What
    =================
    Implemented ftp tasks to gulpfile. Don't need to use an ftp client so much
    now

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,6 +4,9 @@ var watchSass = require("gulp-watch-sass");
 var gs		= require('gulp-selectors');
 var del   = require('del');
 var run   = require('run-sequence');
+var ftp   = require('vinyl-ftp');
+
+var conf  = require('./conf.json');
 
 gulp.task("mangle", ['sass', 'bootstrap'], function() {
   gulp.src(['**/*.css', '**/*.html', '**/*.php', '**/.+(htpasswd|htaccess)', '!dist/**/*.*', '!node_modules/**/*.*'])
@@ -27,12 +30,10 @@ gulp.task("images", function() {
     .pipe(gulp.dest('dist/assets/images/'));
 });
 
-
 gulp.task("js", function() {
   return gulp.src(['assets/js/**/*.*', 'node_modules/bootstrap/dist/js/bootstrap.min.js'])
     .pipe(gulp.dest('dist/assets/js/'));
 });
-
 
 gulp.task("fonts", function() {
   return gulp.src(['assets/fonts/**/*.*'])
@@ -54,4 +55,34 @@ gulp.task("sass:watch", ['sass'], function(){
   .pipe(sass())
   .pipe(gulp.dest("assets/css"));
 });
-//
+
+gulp.task("deploy", function() {
+  var conn = ftp.create({
+    host: conf.ftp.host,
+    user: conf.ftp.user,
+    password: conf.ftp.password,
+    log: console.log
+  });
+
+  return gulp.src('dist/**/*.*', { buffer: false })
+    .pipe(conn.newerOrDifferentSize(conf.ftp.remoteDir))
+    .pipe(conn.dest(conf.ftp.remoteDir));
+});
+
+gulp.task("deploy:test", function() {
+  var conn = ftp.create({
+    host: conf.ftp.host,
+    user: conf.ftp.user,
+    password: conf.ftp.password,
+    log: console.log
+  });
+
+  console.log(conf.ftp, conf.ftp.host);
+
+  return gulp.src('dist/**/*.*')
+    .pipe(conn.newerOrDifferentSize(conf.ftp.remoteDir))
+    .pipe(conn.filter(conf.ftp.remoteDir, function(localFile, remoteFile, cb) {
+      console.log('Local File: ', localFile, 'Remote File: ', remoteFile.ftp.name);
+      cb(null, null);
+    }));
+});
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 1. yarn install
 2. sass:watch
+
+## In order to use `gulp deploy`
+1. Rename `sample.conf.json` to `conf.json`
+2. Fill in the ftp connection details
+3. Run `gulp deploy:test` to test the connection - if the output includes file names that means that the files on the server exist locally also.bitcoin-legend
+4. Run `gulp deploy`
