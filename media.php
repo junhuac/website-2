39dd3acaedebd5976ccce11308601bef38ef8e15
Saborknight <support@saborknight.com>
2017-12-14 20:23:29 +0000

    Feature - Added Media Page
    What
    =================
    Basically just a flexbox of images free wrapping with 33% base.

--- media.php
+++ media.php
@@ -39,7 +39,7 @@
 	gtag('config', 'UA-111023593-1');
 </script>
 
-<header class="header subsite-header events">
+<header class="header subsite-header media">
 	<div class="container">
 		<a href="index.html" class="logo">
 			BetterBetting
@@ -87,111 +87,53 @@
 	</div>
 </header>
 
-<div class="gradient-helper events">
+<div class="gradient-helper media">
 	<div class="right-shape"></div>
 	<div class="container">
-		<div id="first-section">
-			<h1>Meet us at these upcoming Events</h1>
-			<div class="upcoming-event">
-				<div>
-					<div class="logo">
-						<a href="https://blockshowasia.com/">
-							<img src="assets/images/block-show-asia.png" alt="Block Show Asia Logo">
-						</a>
-					</div>
-					<p>Once you open up the Blockchain, you’ll never leave it behind. With the growth of Blockchain technology, it is impossible to ignore the countless advantages it has already given our world. The BlockShow team’s aspiration is to showcase the industry’s most groundbreaking solutions and bring businesses together with the Blockchain.
-					<br />
-					<br />
-					If you yearn to gain real insight into the Blockchain, join our events and avail yourself of the opportunity to meet and partner with the world’s key Blockchain value generators. You will get first-hand practical knowledge of how technology has been successfully implemented in different parts of the world and what a dramatic impact it has already had on the markets.</p>
+		<div>
+			<h1>Media</h1>
+			<div class="media-container">
+				<div class="media-item image">
+					<img src="assets/images/homepage_banner.png" alt="Image of Block Show Asia event">
 				</div>
-				<div class="details">
-					<p class="details-text">November 29 – November 30 2017, <a href="https://goo.gl/maps/oBLYTeQJZgq" title="Go to Google Maps">RWS Sentosa, Singapore</a></p>
-					<div class="register">
-						<a href="" class="button event-register" title="Register to Block Show Asia">Register</a>
+				<div class="media-item image">
+					<img src="assets/images/homepage_banner.png" alt="Image of Block Show Asia event">
+				</div>
+				<div class="media-item video">
+					<div>
+						<iframe width="560" height="315" src="https://www.youtube.com/embed/qzBXJmvpEOM" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
 					</div>
 				</div>
-			</div>
-			<div class="upcoming-event">
-				<div>
-					<div class="logo">
-						<a href="https://hypethon.cryptofriends.io/">
-							<img src="assets/images/hypethon.png" alt="Hypethon Logo">
-						</a>
-					</div>
-					<p>Hypethon on BlockShow is all about networking, and 10 ICO-ready projects will be selected to find their investors, get mentors’ tips and support and finally launch their blockchain products and services right on the spot.
-					<br />
-					<br />
-					Keynote speeches and panel discussions, startups pitching and presentations, networking and experience sharing, partnerships and collaborations — all these opportunities you just can’t miss if you want to be a part of the latest Blockchain industry trends and all of them will be included into the ICO-Hypethon in the City of the Future!</p>
+				<div class="media-item image">
+					<img src="assets/images/homepage_banner.png" alt="Image of Block Show Asia event">
 				</div>
-				<div class="details">
-					<p class="details-text">November 29 – November 30 2017, <a href="https://goo.gl/maps/oBLYTeQJZgq" title="Go to Google Maps">RWS Sentosa, Singapore</a></p>
-					<div class="register">
-						<a href="" class="button event-register" title="Register to Hypethon">Register</a>
+				<div class="media-item image">
+					<img src="assets/images/homepage_banner.png" alt="Image of Block Show Asia event">
+				</div>
+				<div class="media-item video">
+					<div>
+						<iframe width="560" height="315" src="https://www.youtube.com/embed/qzBXJmvpEOM" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+					</div>
+				</div>
+				<div class="media-item image">
+					<img src="assets/images/homepage_banner.png" alt="Image of Block Show Asia event">
+				</div>
+				<div class="media-item image">
+					<img src="assets/images/homepage_banner.png" alt="Image of Block Show Asia event">
+				</div>
+				<div class="media-item video">
+					<div>
+						<iframe width="560" height="315" src="https://www.youtube.com/embed/qzBXJmvpEOM" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div id="second-section">
-			<h1>See the good memories we created</h1>
-			<div class="past-event">
-				<div>
-					<div class="logo">
-						<a href="https://blockshowasia.com/">
-							<img src="assets/images/block-show-asia.png" alt="Block Show Asia Logo">
-						</a>
-					</div>
-					<div class="media-item image">
-						<img src="assets/images/homepage_banner.png" alt="Image of Block Show Asia event">
-					</div>
-					<div class="media-item image">
-						<img src="assets/images/homepage_banner.png" alt="Image of Block Show Asia event">
-					</div>
-					<div class="media-item video">
-						<div>
-							<iframe width="560" height="315" src="https://www.youtube.com/embed/qzBXJmvpEOM" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
-						</div>
-					</div>
-					<div class="more-media category">
-						<a href="media.php?cat=block-show-asia" class="button buy-now" title="See More media from this event!">
-							<img src="assets/images/svg/right_arrow.svg" alt="See More Link">
-							<span>See More</span>
-						</a>
-					</div>
-				</div>
-				<div class="details">
-					<p class="details-text">November 29 – November 30 2017, <a href="https://goo.gl/maps/oBLYTeQJZgq" title="Go to Google Maps">RWS Sentosa, Singapore</a></p>
-				</div>
-			</div>
-			<div class="past-event">
-				<div>
-					<div class="logo">
-						<a href="https://hypethon.cryptofriends.io/">
-							<img src="assets/images/hypethon.png" alt="Hypethon Logo">
-						</a>
-					</div>
-					<div class="media-item image">
-						<img src="assets/images/homepage_banner.png" alt="Image of Block Show Asia event">
-					</div>
-					<div class="media-item image">
-						<img src="assets/images/homepage_banner.png" alt="Image of Block Show Asia event">
-					</div>
-					<div class="more-media category">
-						<a href="media.php?cat=block-show-asia" class="button buy-now" title="See More media from this event!">
-							<img src="assets/images/svg/right_arrow.svg" alt="See More Link">
-							<span>See More</span>
-						</a>
-					</div>
-				</div>
-				<div class="details">
-					<p class="details-text">November 29 – November 30 2017, <a href="https://goo.gl/maps/oBLYTeQJZgq" title="Go to Google Maps">RWS Sentosa, Singapore</a></p>
-				</div>
-			</div>
-		</div>
-		<div class="row more-media">
+		<!-- Can be the Lazy load CTA in the future -->
+		<!-- <div class="row more-media">
 			<div class="col-xs-12 text-center">
 				<a href="media.php" class="button buy-now">More Media</a>
 			</div>
-		</div>
+		</div> -->
 	</div>
 </div>
 
