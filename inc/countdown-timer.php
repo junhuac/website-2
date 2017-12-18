f939f285194854b091e3b3e82f2a45f8189b0d58
Saborknight <support@saborknight.com>
2017-12-18 11:17:27 +0000

    Remove countdown timer
    Why
    =================
    We're not counting down to anything anymore
    However, I've salvaged the code for future use within countdown-timer.php

---
+++ inc/countdown-timer.php
@@ -0,0 +1,19 @@
+<div class="container">
+	<div class="countdown">
+		<h3>Time left to Token Sale</h3>
+		<div id="timer-js" class="timer">
+			<span class="days">30</span>
+			<span class="hours">14</span>
+			<span class="minutes">27</span>
+			<span class="sec">53</span>
+		</div>
+		<div class="timer-description">
+			<p>
+				<strong>DAYS</strong>
+				Hours
+				Minutes
+				Seconds
+			</p>
+		</div>
+	</div>
+</div>
